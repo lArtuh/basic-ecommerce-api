07e8914d9baa35257241ebb59374f7f13e285768
lArtuh <arturogtx12@gmail.com>
2025-07-31 11:41:19 -0500

    main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import products, categories, orders
-from app.database import Base, engine
-
+from app.database import engine
+from app.models import Base
 
 app = FastAPI()
 
